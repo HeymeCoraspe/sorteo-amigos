aa3b9915fe4895ed7e619cc569d6b33c0b7eb840
Heyme <heyme.coraspe@gmail.com>
2025-02-11 23:24:42 -0300

    updateList added, addFriends modified

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 friendsList=[];
 
 function addFriends(){
-    let friendName= document.getElementById("input-name").value;
-    friendsList.push(friendName);
-    //console.log(friendsList)
+    let friendName= document.getElementById("input-name").value.toLowerCase();
+
+    if (friendName==""){
+        alert("Debes agregar un nombre");
+        } else{
+            if (friendsList.includes(friendName)){
+                alert("Ya agregaste ese nombre");
+            }else{
+                friendsList.push(friendName);
+                return friendName;
+        };
+    };
+};    
+       
+function updateList(){
+    let newListElement= document.querySelector(".name-list");
+    newListElement.innerHTML="";
+
+    friendsList.forEach(name => {
+        let listItem= document.createElement("li");
+        listItem.textContent=name;
+        newListElement.append(listItem);
+    });    
 };
 
 function cleanField(){
@@ -15,4 +35,7 @@ function cleanField(){
 document.getElementById("input-bt-add").addEventListener("click", ()=>{
     addFriends();
     cleanField();
+    updateList();
 });
+
+/**updateList added, addFriends modified */
